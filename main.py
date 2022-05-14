40844871a55771e29ac8ca7fa3a0541d95c14dc5
CARLOS SAMUEL SAMUEL <104873137+Samuel13452435@users.noreply.github.com>
2022-05-14 01:12:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,179 @@
+#print("digite sus datos")
+#Altura = float(input("Digite Altura: \n"))
+#Base = float (input("Digite Base: \n"))
+#Area = Altura+Base /2
+#print("El area de un triangulo es: " +str(Area)) 
+
+#Ejercicio 2
+#print("digite sus datos")
+#Num1 = float(input(" digite numero 1: \n"))
+#Num2 = float(input("Digite numero 2: \n"))
+#Operacion = Num1 + Num2
+#print("El resultado es: " + str(Operacion))
+
+#Ejercicio 3
+#print("digite sus datos")
+#Num1 = int(input("digite numero: \n"))
+#Operacion = Num1 **2
+#print("El resultado es: " + str(Operacion))
+
+#Ejercicio 4
+#print("Convertidor de euros a dolares")
+#Euros = float(input("Digite cantidad de euros: \n"))
+#Operacion = Euros * 1.05
+#print("La conversion de euros a dolares es " + str(Operacion))
+
+#Ejercicio 5
+#print("Digite sus datos")
+#Lado = int(input("Digite el lado del cuadrado \n"))
+#Area = Lado **2 
+#Perimetro= Lado * 4 
+#print("El area de un cuadrado es " +str(Area))
+#print("El perimetro de un cuadrado es " +str(Perimetro))
+
+#Ejercicio 6
+#print("Digite sus datos")
+#Radio = float(input("Digite el radio: \n"))
+#Altura = float(input("Digite la altura: \n"))  
+#Area =  (3.14 *2) * (Radio) * (Radio + Altura)
+#Volumen  = (3.14 * Altura) * (Radio **2)
+#print("El Area de un cilindro es " +str(Area)) 
+#print("El Volumen de un cilindro es " +str(Volumen))
+
+#Ejercicio 7
+#print("Digite sus datos")
+#Radio = float(input("Digite el Radio \n"))
+#Longitud = Radio * (3.14 *2)
+#Area = Radio **2 * (3.14)
+#print("La longitud de una circunferencia es " +str(Longitud))
+#print("El Area de una circunferencia es " +str(Area))
+
+#Ejercicion 8
+#print("Digite sus datos")
+#Num1 = float(input("Digite numero 1 \n"))
+#Num2 = float(input("Digite numero 2 \n"))
+#Num3 = float(input("Digite numero 3 \n"))
+#Promedio = (Num1 + Num2 + Num3) /3
+#print("El promedio es " +str(Promedio))
+
+#2.Condicionales
+#Ejercicio 1
+#print("Digite sus datos")
+#if (Numero>0): print("El numero es positivo")
+#if (Numero<0): print("El numero es negativo")
+
+#Ejercicio 2
+#print("Digite sus datos")
+#Num1 = float(input("Digite el primer numero \n"))
+#um2 = float(input("Digite el segundo numero \n"))
+#if (Num1>Num2): print("El primer numero es mayor al segundo \n")
+#if (Num2>Num1): print("El segundo numero es mayor al primero \n")
+
+#Ejercicio 3
+#print("Digite sus datos")
+#Num1 = float(input("Digite el primer numero \n"))
+#Num2 = float(input("Digite el segundo numero \n"))
+#Num3 = float(input("Digite el tercer numero \n"))
+#list = [Num1,Num2,Num3]
+#list.sort()
+#menor=(list[0])
+#mayor=(list[2])
+#print("El numero menor es " ,str(menor) , "El numero mayor es" ,str(mayor))
+
+#Ejercicio 4
+#print("Digite sus datos")
+#A = float(input("Digite numero 1 \n"))
+#B = float(input("Digite numero 2 \n"))
+#if (A<B): 
+  #   print("El resultado es " + "" + str(A+B))
+#else:
+   #print("El resultado es " + str(A-B))
+
+  #Ejercicio 5
+#print("Digite sus datos")
+#Num1 = float(input("Digite el primer numero  \n"))
+#Num2 = float(input("Digite el segundo numero \n"))
+#if (Num2 <= 0):
+  #print("La division no es posible")
+#else:
+  #print("El resultado es " + str(Num1/Num2))
+
+#Ejercicio 6
+#print("Digite sus datos")
+#A = float(input("Digite numero 1 \n"))      
+#B = float(input("Digite numero 2 \n"))
+#if ((B ) < 0):
+ #print("El resultado es " + str(A+B))
+#f((A) < 0 ):
+  #print("El resultado es " + str(A+B))
+#else:
+ #if ((A and B) > 0):
+  # print("El resultado es " + str(A*B))
+
+#Ejercicio 7
+#año = int(input("Digite el año \n"))
+#if año % 4 == 0 and (año % 100 != 0 or año % 400 == 0):
+#	print("Es bisiesto")
+#else:  
+ # print("No es bisiesto")
+
+#3.Ciclos.
+
+#Ejercicio 1
+#Numeros = 3
+#while Numeros < 100:
+ #print(Numeros)
+ #Numeros +=3
+
+  #Ejercicio 2
+#Numeros = 1
+#while Numeros < 100:
+  #print(Numeros)
+  #Numeros +=2
+
+#Ejercicio 3
+#Numeros = 2
+#while Numeros < 102:
+  #print(Numeros)
+  #Numeros +=2
+
+#Ejercicio 4
+#for x in range(1,31,1):
+  #x = x **2
+  #print(x)
+
+#Ejercicio 5
+#total = 0
+#for x in range (1,101,1):
+#  x = x **2 
+ # total = total + x
+  #print( str(total))
+
+#Ejercicio 6
+#print("Digite sus datos")
+#a = int(input("Digite el primer numero \n"))
+#b = int(input("Digite el segundo numero \n"))
+#for x in range (a +1,b,1):
+ #if (a<b):
+  #print("  " + str(x))
+
+#Ejercicio 7
+#a = 1
+#b = 0
+#while a !=0 : 
+  #c = int(input("Digite sus numeros \n"))
+  #if c == 0 :
+    #a = 0
+  #else :
+     # b = b + c
+      #print(b)
+      #print("El resultado es " , str(b))
+
+    
+
+
+
+
+
+  
+  
